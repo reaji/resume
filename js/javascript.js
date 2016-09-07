45c92927d3bc5a79348267124dfe26958299f79b
reaji <R@douBles-MacBook-Pro.local>
2016-09-06 19:28:31 -0700

    new intro page.

--- js/javascript.js
+++ js/javascript.js
@@ -12,14 +12,10 @@ $(document).ready(function(){
 		
 	});
 
-// 	$( window ).scroll(function() {
-// 		if( scroll = false ){
-// 		$( '#navbackground' ).css( "display", "none" );
-// 	}else{
-// 	$( '#navbackground' ).css( "box-shadow", "0 2px 2px 0 rgba(0, 0, 0, 0.1), 0 3px 4px 0 rgba(0, 0, 0, 0.15" );
-// 	}
-// });
 
+$(window).mouseenter(function(){
+	$('.responsiveback, .responsivenote').fadeOut("slow");
+});
 
 $(window).scroll(_.debounce(function(){
     $('#navbackground').css("box-shadow", "rgba(0,0,0,0.16) 0 2px 4px");
@@ -29,7 +25,4 @@ $(window).scroll(_.debounce(function(){
     $('#navbackground').css("box-shadow", "none");
 }, 150));
 
-// 	$( window ).end( "scroll" ,function() {
-//   $( '#navbackground' ).css( "display", "none" );
-// });
 });
